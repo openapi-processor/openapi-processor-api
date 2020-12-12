cd00df4f4f59e092c458bb3131189059481e3a5f
Martin Hauner <martin.hauner@gmx.net>
2020-12-12 16:51:18 +0100

    javadoc complains

--- src/main/java/io/openapiprocessor/api/v1/Version.java
+++ src/main/java/io/openapiprocessor/api/v1/Version.java
@@ -1,12 +1,12 @@
 /*
- * Copyright © 2020 https://github.com/openapi-processor/openapi-processor-api
+ * Copyright 2020 https://github.com/openapi-processor/openapi-processor-api
  * PDX-License-Identifier: Apache-2.0
  */
 
 package io.openapiprocessor.api.v1;
 
 /**
- * (experimental & optional) interface to provide version information of the processor.
+ * (experimental and optional) interface to provide version information of the processor.
  */
 public interface Version {
 
