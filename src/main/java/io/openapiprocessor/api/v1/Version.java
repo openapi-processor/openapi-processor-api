d396cabdb94217e44cb4530447ce1633e3e98f30
Martin Hauner <martin.hauner@gmx.net>
2020-12-12 16:38:54 +0100

    experimental version interface

---
+++ src/main/java/io/openapiprocessor/api/v1/Version.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2020 https://github.com/openapi-processor/openapi-processor-api
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiprocessor.api.v1;
+
+/**
+ * (experimental & optional) interface to provide version information of the processor.
+ */
+public interface Version {
+
+    /**
+     * The version string of the openapi-processor.
+     *
+     * @return the version string of the openapi-processor
+     */
+    String getVersion();
+
+}
