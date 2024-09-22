e5791d4e5b4703ffa92c2e9d7b70a8508519e812
Martin Hauner <martin.hauner@gmx.net>
2024-09-22 10:46:06 +0200

    add test api

---
+++ src/main/java/io/openapiprocessor/api/v2/OpenApiProcessorTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2024 https://github.com/openapi-processor/openapi-processor-test
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiprocessor.api.v2;
+
+/**
+ * (integration) test support.
+ * provides the destination folders in the targetDir depending on the targetDir layout.
+ */
+public interface OpenApiProcessorTest {
+    /**
+     * provides the target source root folder in targetDir.
+     *
+     * @return the source root below the targetDir or null if the targetDir is the source root.
+     */
+    String getSourceRoot();
+
+    /**
+     * provides the target resource root folder in targetDir.
+     *
+     * @return the resource root below the targetDir or null.
+     */
+    String getResourceRoot();
+}
